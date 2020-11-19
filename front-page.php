79129dea1ef500c3fa360f526ef4e61ba3bd5ba6
StanislavPaladin <Paladinstanis@gmail.com>
2020-11-20 00:12:40 +0300

    WIP lesson 12

---
+++ front-page.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+<main>
+  <div class="container">
+  <h1>12 урок. Таймкод: 22:05</h1>
+    <div class="hero">
+      <div class="left">
+        <img src="<?php get_template_directory_uri() . '/assets/images/image.jpg' ?>" alt="" class="post-thumb">
+        <a href="#" class="author">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/avatar.png' ?>" alt="" class="avatar">
+          <div class="author-bio">
+            <span class="author-name">Имя Автора</span>
+            <span class="author-rank">Должность</span>
+          </div>
+        </a>
+        <div class="post-text">
+          <a href="#" class="cathegory-name">Рубрики</a>
+          <h2 class="post-title">Название поста</h2>
+          <a href="#" class="more">Читать далее</a>
+        </div>
+      </div>
+      <div class="right">
+
+      </div>
+    </div>
+  </div>
+</main>
