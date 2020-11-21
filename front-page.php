f7080d98c662b099e4fe7f03d4285edbead316a8
StanislavPaladin <Paladinstanis@gmail.com>
2020-11-21 23:39:05 +0300

    feat: created thumbnails-section dynamic

--- front-page.php
+++ front-page.php
@@ -1,25 +1,73 @@
 <?php get_header(); ?>
-<main>
+<main class="front-page-header">
   <div class="container">
-  <h1>12 урок. Таймкод: 22:05</h1>
     <div class="hero">
       <div class="left">
-        <img src="<?php get_template_directory_uri() . '/assets/images/image.jpg' ?>" alt="" class="post-thumb">
-        <a href="#" class="author">
-          <img src="<?php echo get_template_directory_uri() . '/assets/images/avatar.png' ?>" alt="" class="avatar">
+            <?php
+      global $post;
+
+      $myposts = get_posts([ 
+        'numberposts' => 1,
+        'category_name'      => 'javascript',
+      ]);
+
+      if( $myposts ){
+        foreach( $myposts as $post ){
+          setup_postdata( $post );
+          ?>
+      <img src="<?php the_post_thumbnail_url( )?>" alt="" class="post-thumb">
+      <?php $author_id = get_the_author_meta('ID'); ?>
+        <a href="<?php echo get_author_posts_url($author_id); ?>" class="author">
+          <img src="<?php echo get_avatar_url($author_id) ?>" alt="" class="avatar">
           <div class="author-bio">
-            <span class="author-name">Имя Автора</span>
+            <span class="author-name"><?php the_author(); ?></span>
             <span class="author-rank">Должность</span>
           </div>
         </a>
         <div class="post-text">
-          <a href="#" class="cathegory-name">Рубрики</a>
-          <h2 class="post-title">Название поста</h2>
-          <a href="#" class="more">Читать далее</a>
+          <?php the_category(); ?>
+          <h2 class="post-title"><?php the_title(); ?></h2>
+          <a href="<?php echo get_the_permalink(); ?>" class="more">Читать далее</a>
         </div>
+                    <?php 
+              }
+            } else {
+              // Постов не найдено
+              ?> <p class="">Постов не найдено</p> <?php
+            }
+            wp_reset_postdata(); // Сбрасываем $post
+            ?>
       </div>
       <div class="right">
+        <h3 class="recommend">Рекомендуем</h3> 
+        <ul class="posts-list">
+        <?php
+      global $post;
 
+      $myposts = get_posts([ 
+        'numberposts' => 5,
+        'offset' => 1,
+      ]);
+
+      if( $myposts ){
+        foreach( $myposts as $post ){
+          setup_postdata( $post );
+          ?>
+          <li class="post">
+            <?php the_category(); ?>
+            <a class="post-permalink" href="<?php get_the_permalink(); ?>" class="">
+            <h4 class=""><?php the_title(); ?></h4>
+            </a>
+          </li>
+          <?php 
+              }
+            } else {
+              // Постов не найдено
+              ?> <p class="">Постов не найдено</p> <?php
+            }
+            wp_reset_postdata(); // Сбрасываем $post
+            ?>
+        </ul>
       </div>
     </div>
   </div>
